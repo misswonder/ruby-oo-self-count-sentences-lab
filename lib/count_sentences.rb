60d5025f2dca455194e2f871b0868d81a7bc5277
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-13 20:04:07 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,42 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string
+  end 
+
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false 
+    end 
   end
 
   def count_sentences
-
+    # [a-z]
+    # [abc]
+    # [.?!]
+    # + - one or more
+    # * - zero or more
+    self.split(/[.?!]+/).count
   end
 end
